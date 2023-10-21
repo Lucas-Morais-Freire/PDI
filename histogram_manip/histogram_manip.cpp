603efa77d3bbfb523277dbe7cf281ce52e26bbf8
Lucas Morais Freire <lucasmoraisf@gmail.com>
2023-10-20 21:13:23 -0300

    started dev on dftimage

--- histogram_manip/histogram_manip.cpp
+++ histogram_manip/histogram_manip.cpp
@@ -3,15 +3,10 @@
 
 int main(int argc, char** argv){
     cv::Mat image;
+    cv::Mat imageBRG;
     int width, height;
     cv::VideoCapture cap;
-    std::vector<cv::Mat> planes;
-    cv::Mat histR, histG, histB;
-    int nbins = 64;
-    float range[] = {0, 255};
-    const float *histrange = { range };
-    bool uniform = true;
-    bool acummulate = false;
+    std::vector<int> hist(256, 0);
     int key;
 
     cap.open(0);
@@ -29,36 +24,59 @@ int main(int argc, char** argv){
     std::cout << "largura = " << width << std::endl;
     std::cout << "altura  = " << height << std::endl;
 
-    int histw = nbins, histh = nbins/2;
-    cv::Mat histImgR(histh, histw, CV_8UC3, cv::Scalar(0,0,0));
-    cv::Mat histImgG(histh, histw, CV_8UC3, cv::Scalar(0,0,0));
-    cv::Mat histImgB(histh, histw, CV_8UC3, cv::Scalar(0,0,0));
+    // a fim de mostrar a comparacao, duas janelas diferentes serao criadas.
+    cv::namedWindow("equalizada");
+    cv::namedWindow("normal");
+
+    // este sera o vetor da funcao distribuicao acumulada
+    std::vector<int> cdf(256, 0);
 
     while(1){
-        cap >> image;
-        cv::split (image, planes);
-        cv::calcHist(&planes[0], 1, 0, cv::Mat(), histB, 1, &nbins, &histrange, uniform, acummulate);
-        cv::calcHist(&planes[1], 1, 0, cv::Mat(), histG, 1, &nbins, &histrange, uniform, acummulate);
-        cv::calcHist(&planes[2], 1, 0, cv::Mat(), histR, 1, &nbins, &histrange, uniform, acummulate);
-        cv::normalize(histR, histR, 0, histImgR.rows, cv::NORM_MINMAX, -1, cv::Mat());
-        cv::normalize(histG, histG, 0, histImgG.rows, cv::NORM_MINMAX, -1, cv::Mat());
-        cv::normalize(histB, histB, 0, histImgB.rows, cv::NORM_MINMAX, -1, cv::Mat());
+        // capturar a imagem colorida
+        cap >> imageBRG;
 
-        histImgR.setTo(cv::Scalar(0));
-        histImgG.setTo(cv::Scalar(0));
-        histImgB.setTo(cv::Scalar(0));
+        // converter a imagem colorida para escala de cinza
+        cv::cvtColor(imageBRG, image, cv::COLOR_BGR2GRAY);
 
-        for(int i=0; i<nbins; i++){
-            cv::line(histImgR, cv::Point(i, histh), cv::Point(i, histh-cvRound(histR.at<float>(i))), cv::Scalar(0, 0, 255), 1, 8, 0);
-            cv::line(histImgG, cv::Point(i, histh), cv::Point(i, histh-cvRound(histG.at<float>(i))), cv::Scalar(0, 255, 0), 1, 8, 0);
-            cv::line(histImgB, cv::Point(i, histh), cv::Point(i, histh-cvRound(histB.at<float>(i))), cv::Scalar(255, 0, 0), 1, 8, 0);
+        // mostrar a imagem colorida
+        cv::imshow("normal", image);
+
+        // zerar o histograma
+        for (int i = 0; i <= 255; i++) {
+            hist[i] = 0;
         }
-        histImgR.copyTo(image(cv::Rect(0, 0       ,nbins, histh)));
-        histImgG.copyTo(image(cv::Rect(0, histh   ,nbins, histh)));
-        histImgB.copyTo(image(cv::Rect(0, 2*histh ,nbins, histh)));
-        cv::imshow("image", image);
+        
+        // calcular o histograma
+        for (int i = 0; i < image.rows; i++) {
+            for (int j = 0; j < image.cols; j++) {
+                hist[(int)image.at<uchar>(i,j)]++;
+            }
+        }
+
+        // calcular a cdf
+        cdf[0] = hist[0];
+        
+        for (int i = 1; i <= 255; i++) {
+            cdf[i] = hist[i] + cdf[i - 1];
+        }
+
+        // normalizamos a cdf para obter a tabela:
+        for (int i = 0; i <= 255; i++) {
+            cdf[i] = (cdf[i]*255)/cdf[255];
+        }
+
+        // aplicamos a tabela e salvamos na propria imagem
+        for (int i = 0; i < image.rows; i++) {
+            for (int j = 0; j < image.cols; j++) {
+                image.at<uchar>(i, j) = (uchar)cdf[(int)image.at<uchar>(i, j)];
+            }
+        }
+
+        cv::imshow("equalizada", image);
         key = cv::waitKey(30);
         if(key == 27) break;
+        l++;
     }
+
     return 0;
 }
